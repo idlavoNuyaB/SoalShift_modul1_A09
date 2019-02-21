00839bb32451896532ed1aa7ce9585d640d8c892
idlavoNuyaB <36811359+idlavoNuyaB@users.noreply.github.com>
2019-02-21 23:47:54 +0700

    v4

--- soal4.sh
+++ soal4.sh
@@ -12,10 +12,6 @@ kecil=$(printf \\$(printf '%03o' $kecil))
 #batas atas z ke huruf lain dengan mengembalikin ascii ke decimal lalu ke ascii lagi
 bawah=$(printf '%d' "'$kecil")
 let bawah--
-if [ $bawah -lt 97 ]
-then
-  bawah=122
-fi
 bawah=$(printf \\$(printf '%03o' $bawah))
 
 #batas bawah A ke huruf lain decimal ascii
@@ -24,10 +20,6 @@ besar=$(printf \\$(printf '%03o' $besar))
 #batas atas Z ke huruf lain ascii decimal ascii
 atas=$(printf '%d' "'$besar")
 let atas--
-if [ $atas -lt 65 ]
-then
-    atas=90
-fi
 atas=$(printf \\$(printf '%03o' $atas))
 
 jam=`date "+%X" | awk -F: '{print $1}'`
@@ -44,34 +36,18 @@ cat /var/log/syslog | tr [a-z] ["$kecil"-za-"$bawah"] | tr [A-Z] ["$besar"-ZA-"$
 let waktu--
 #batas bawah huruf kecil decimal ascii
 dkecil=$((122-$waktu))
-if [ $dkecil -gt 122 ]
-then
-    a=97
-fi
 dkecil=$(printf \\$(printf '%03o' $dkecil))
 #batas atas huruf kecil ascii decimal ascii
 dbawah=$(printf '%d' "'$dkecil")
 let dbawah--
-if [ $dbawah -lt 97 ]
-then
-    dbawah=122
-fi
 dbawah=$(printf \\$(printf '%03o' $dbawah))
 
 #batas bawah huruf besar decimal ascii
 cbesar=$((90-$waktu))
-if [ $cbesar -gt 90 ]
-then
-    cbesar=65
-fi
 cbesar=$(printf \\$(printf '%03o' $cbesar))
 #batas atas huruf besar ascii decimal ascii
 datas=$(printf '%d' "'$cbesar")
 let datas--
-if [ $datas -lt 65 ]
-then
-    datas=90
-fi
 datas=$(printf \\$(printf '%03o' $datas))
 
 cat "$jam:$menit $tanggal-$bulan-$tahun".txt | tr [a-z] ["$dkecil"-za-"$dbawah"] | tr [A-Z] ["$cbesar"-ZA-"$datas"] > "$jam:$menit $tanggal-$bulan-$tahun-decrypted".txt
